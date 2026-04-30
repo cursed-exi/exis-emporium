e9b61efbf68e2091039e01ab11b11a3d294cca6b
cursed-exi <andrzejsarosiek21@gmail.com>
2026-04-30 13:03:10 +0200

    Add articles for 2026 with titles and descriptions

---
+++ data/articles/2026.js
@@ -0,0 +1,34 @@
+[
+  {
+    "id": "los_kerdrim",
+    "title": "Land of Sain: Kerdrim",
+    "date": "2026-04-29",
+    "category": "Land of Sain",
+    "description": "A short introduction to my setting: Land of Sain - a description of village: Kerdrim",
+    "link": "../Articles/2026/los_kerdrim.html"
+  },
+  {
+    "id": "unbound_gear",
+    "title": "Unbound Gear",
+    "date": "2026-04-22",
+    "category": "Mechanics Discussion",
+    "description": "A system for evolving weapons with flavour and utility.",
+    "link": "../Articles/2026/unbound_gear.html"
+  },
+  {
+    "id": "atomized_networks",
+    "title": "Atomized Networks",
+    "date": "2026-04-15",
+    "category": "Worldbuilding",
+    "description": "How to make worlds feel alive and cooperative.",
+    "link": "../Articles/2026/atomized_networks.html"
+  },
+  {
+    "id": "breaking_the_npcs",
+    "title": "Breaking the NPCs",
+    "date": "2026-04-08",
+    "category": "Mechanics Discussion",
+    "description": "Legendary Break System and boss design.",
+    "link": "../Articles/2026/breaking_the_npcs.html"
+  }
+]
